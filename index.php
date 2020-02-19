bf34982ccee5a733a90ea3e5ce462da469943ab6
sebaspala02 <sebastianpala02@gmail.com>
2020-02-19 17:02:28 -0500

    inicio

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Fincas</title>
+    <title>INICIO</title>
     <link rel="stylesheet" href="resource/styles/bootstrap.min.css">
 
     <script type="text/javascript" src="resource/jquery/jquery.js"></script>
